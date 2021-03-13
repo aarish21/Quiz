6c5f6de34c03cace26911541a3502a986202417f
tmnabeel30 <tmnabeel30@gmail.com>
2021-03-14 03:08:44 +0530

    Font

--- QuizApp/Controller/ViewController.swift
+++ QuizApp/Controller/ViewController.swift
@@ -20,12 +20,15 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        questionImage.layer.cornerRadius = 15.0
+        questionImage.clipsToBounds  = true
+        
         // Do any additional setup after loading the view.
         aButton.setTitle(ques[quesNumber][1], for: .normal)
         bButton.setTitle(ques[quesNumber][2], for: .normal)
         cButton.setTitle(ques[quesNumber][3], for: .normal)
         dButton.setTitle(ques[quesNumber][4], for: .normal)
-        questionImage.image = #imageLiteral(resourceName: "vlcsnap-2013-11-14-16h57m44s17")
+        questionImage.image = #imageLiteral(resourceName: "avatars-000318285440-0dn57i-t500x500")
         questionLable.text = ques[0][0]
         updateUI()
     }
